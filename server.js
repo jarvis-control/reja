5c6ad92af2d8df97135e570f7bb365b0379088b4
jarvis-control <shakh_mac3@icloud.com>
2025-12-25 20:30:55 +0900

    feat: develop author router ejs page

--- server.js
+++ server.js
@@ -2,6 +2,16 @@ console.log("Server is starting");
 const express = require("express");
 const app = express();
 const http = require("http");
+const fs = require("fs");
+
+let user;
+fs.readFile("database/user.json", "utf-8", (err, data) => {
+    if (err) {
+        console.log("ERROR:", err);
+    } else {
+        user = JSON.parse(data)
+    }
+});
 
 // 1 Entry code
 app.use(express.static("public"));
@@ -16,9 +26,13 @@ app.set("view engine", "ejs");
 
 // 4 Routing code
 app.post("/create-item", (req, res) => {
-    console.log(req);
+    console.log(req.body);
     res.json({ test: "success"});
 });
+
+app.get('/author', (req, res) => {
+    res.render("author", { user: user });
+})
 
 app.get("/", function (req, res) {
     res.render("xarid");
