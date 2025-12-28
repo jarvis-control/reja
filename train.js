2a42340a67e721fdb07c07942525b98e41d99aeb
jarvis-control <shakh_mac3@icloud.com>
2025-12-28 19:24:32 +0900

    feat: Callback & ASYN function & TASK A

--- train.js
+++ train.js
@@ -1,1 +1,104 @@
-console.log("Train.js file is running");
+// TASK A: 
+function countLetter(letter, word) {
+    let count = 0;
+    for (let i = 0; i < word.length; i++) {
+        if (word[i] === letter) {
+            count++;
+        } 
+    }
+    return count;
+}
+
+console.log(countLetter("e", "engineer"));
+
+
+
+// console.log("Jack Ma maslahatlari");
+// const list = [
+//   "Yaxshi talaba bo'ling", // 0–20
+//   "To'g'ri boshliq tanlang...", // 20–30
+//   "O'zingizga ishlaringizni boshlang", // 30–40
+//   "Siz kuchli bo'lgan narsalarni qiling", // 40–50
+//   "Yoshlarga investitsiya qiling", // 50–60
+//   "Endi dam oling, endi foydasi yo'q", // 60+
+// ];
+
+// /* Deine via Callback functione: */
+// function maslahatBering(a, callback) {
+//     if(typeof a !== 'number') callback("Please, insert a number", null);
+//     else if (a <= 20) callback(null, list[0]);
+//     else if (a > 20 && a <= 30) callback(null, list[1]);
+//     else if (a > 30 && a <= 40) callback(null, list[2]);
+//     else if (a > 40 && a <= 50) callback(null, list[3]);
+//     else if (a > 50 && a <= 60) callback(null, list[4]);
+//     else {
+//         // setTimeout(function () {
+//         //     callback(null, list[4]);
+//         // }, 5000);
+
+//         /* Callback ni o'rni (20:17) */
+//         setInterval(function () {
+//             callback(null, list[5]);
+//         }, 1000);
+//     }
+// }
+
+// /* Call via Callback function: */
+// console.log("passed here 0");
+
+// maslahatBering(65, (err, data) => {
+//     if (err) console.log("ERROR:", err);
+//     else {
+//         console.log("maslahatni biri:", data);
+//     }
+// })
+
+// console.log("passed here 1");
+
+/* Define via Async function: */
+// async function maslahatBering(a) {
+//   if (typeof a !== "number") throw new Error("Please, insert a number");
+//   else if (a <= 20) return list[0];
+//   else if (a > 20 && a <= 30) return list[1];
+//   else if (a > 30 && a <= 40) return list[2];
+//   else if (a > 40 && a <= 50) return list[3];
+//   else if (a > 50 && a <= 60) return list[4];
+//   else {
+//     /* Case 1: setTimeOut doesn't work with Async */
+//     // setTimeout(function () {
+//     //     return list[5];
+//     // }, 5000);
+
+//     /* Case 3: */
+//     // return list[5];
+
+//     /* Case 3: setTimeOut works with Async using Promise */
+//     return new Promise((resolve, reject) => {
+//       setTimeout(() => {
+//         resolve(list[5]);
+//       }, 5000);
+//     });
+//   }
+// }
+
+/* Call via .then() & .catch(): */
+// console.log("passed here 0");
+// maslahatBering(25)
+//   .then((data) => {
+//     console.log("maslahatni biri:", data);
+//   })
+//   .catch((err) => {
+//     console.log("ERROR:", err);
+//   });
+// console.log("passed here 1");
+
+/* Call via async function (await): */
+// async function run() {
+//   let maslahat = await maslahatBering(65);
+//   console.log(maslahat);
+//   maslahat = await maslahatBering(31);
+//   console.log(maslahat);
+//   maslahat = await maslahatBering(41);
+//   console.log(maslahat);
+// }
+// run();
