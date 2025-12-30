a051eff8244df683523b40b6d0b78ee2bac7f5d2
jarvis-control <shakh_mac3@icloud.com>
2025-12-30 21:14:10 +0900

    feat: mongoDB ulanish logic

--- server.js
+++ server.js
@@ -1,45 +1,30 @@
-console.log("Server is starting");
-const express = require("express");
-const app = express();
 const http = require("http");
-const fs = require("fs");
+const mongodb = require("mongodb");
 
-let user;
-fs.readFile("database/user.json", "utf-8", (err, data) => {
-  if (err) {
-    console.log("ERROR:", err);
-  } else {
-    user = JSON.parse(data);
+let db;
+const connectionString =
+  "mongodb+srv://jarvis:MySzOVSaZspgHlRm@cluster0.yiasqur.mongodb.net/";
+
+mongodb.connect(
+  connectionString,
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  },
+  (err, client) => {
+    if (err) console.log("ERROR on connection");
+    else {
+      console.log("MongoDB successful connection");
+      module.exports = client;
+
+      const app = require("./app");
+      const server = http.createServer(app);
+      let PORT = 3000;
+      server.listen(PORT, function () {
+        console.log(
+          `The server is running on ${PORT}, http://localhost:${PORT}`
+        );
+      });
+    }
   }
-});
-
-// 1 Entry code
-app.use(express.static("public"));
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-
-// 2 Session code
-
-// 3 Views code
-app.set("views", "views");
-app.set("view engine", "ejs");
-
-// 4 Routing code
-app.post("/create-item", (req, res) => {
-  console.log(req.body);
-  res.json({ test: "success" });
-});
-
-app.get("/author", (req, res) => {
-  res.render("author", { user: user });
-});
-
-app.get("/", function (req, res) {
-  res.render("reja");
-});
-
-const server = http.createServer(app);
-let PORT = 3000;
-server.listen(PORT, function () {
-  console.log(`The server is running on ${PORT}, http://localhost:${PORT}`);
-});
+);
