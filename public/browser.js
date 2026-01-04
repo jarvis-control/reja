3529a8b57130ed1a01cf0020b20dcd4a0f875420
jarvis-control <shakh_mac3@icloud.com>
2026-01-04 19:30:48 +0900

    feat: Reja loyihamizning yakuniy bosqich logic (2-attempt)

--- public/browser.js
+++ public/browser.js
@@ -90,7 +90,7 @@ document.addEventListener("click", function (e) {
 });
 
 document.getElementById("clean-all").addEventListener("click", function () {
-  axios.post("/delete-all", { delete_all: true }).then((respone) => {
+  axios.post("/delete-all", { delete_all: true }).then((response) => {
     alert(response.data.state);
     document.location.reload();
   });
